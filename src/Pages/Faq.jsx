fda8f7ba5cbd471d469c7f541dd87fee4c22d099
Davi Komura de Castro <davi.komura@gmail.com>
2024-10-08 19:24:20 +0100

    Finalizacao page faq

--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
+import faqsData from "/data.json";
 
 export const Faq = () => {
+
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [faqs, setFaqs] = useState([]);
+
+  useEffect(() => {
+    // Load the FAQs from the JSON file
+    setFaqs(faqsData.faqs);
+  }, []);
+
+  const toggleAccordion = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
-    <div>Faq</div>
+    <section className="py-24">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mb-16">
+          <h6 className="text-lg text-indigo-600 font-medium text-center mb-2">
+            FAQs
+          </h6>
+          <h2 className="text-4xl font-manrope text-center font-bold text-gray-900 leading-[3.25rem]">
+            Frequently asked questions
+          </h2>
+        </div>
+
+        <div className="accordion-group">
+          {faqs.map((faq, index) => (
+            <div
+              key={index}
+              className={`accordion py-8 px-6 border-b border-solid border-gray-200 transition-all duration-500 rounded-2xl ${
+                activeIndex === index
+                  ? "hover:bg-indigo-50 accordion-active:bg-indigo-50 active"
+                  : "hover:bg-indigo-50"
+              }`}
+            >
+              <button
+                className={`accordion-toggle group inline-flex items-center justify-between leading-8 text-gray-900 w-full transition duration-500 text-left hover:text-indigo-600 ${
+                  activeIndex === index
+                    ? "accordion-active:font-medium accordion-active:text-indigo-600"
+                    : ""
+                }`}
+                onClick={() => toggleAccordion(index)}
+                aria-controls={`basic-collapse-${index}`}
+              >
+                <h5>{faq.question}</h5>
+                <svg
+                  className={`text-gray-500 transition duration-500 group-hover:text-indigo-600 ${
+                    activeIndex === index ? "rotate-180" : ""
+                  }`}
+                  width="22"
+                  height="22"
+                  viewBox="0 0 22 22"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M16.5 8.25L12.4142 12.3358C11.7475 13.0025 11.4142 13.3358 11 13.3358C10.5858 13.3358 10.2525 13.0025 9.58579 12.3358L5.5 8.25"
+                    stroke="currentColor"
+                    strokeWidth="1.6"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                </svg>
+              </button>
+              <div
+                id={`basic-collapse-${index}`}
+                className="accordion-content w-full px-0 overflow-hidden"
+                style={{
+                  maxHeight: activeIndex === index ? "250px" : "0",
+                  transition: "max-height 0.5s ease",
+                }}
+              >
+                <p className="text-base text-gray-900 leading-6">
+                  {faq.answer}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
--- src/Pages/TermsConditions.jsx
+++ src/Pages/TermsConditions.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export const TermsConditions = () => {
+
   return (
     <div>TermsConditions</div>
   )
